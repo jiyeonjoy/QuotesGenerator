f7b10434cfcb28ccb7e675f90d133ba824ead574
최지연 <jyc@joonggonara.co.kr>
2022-10-19 17:12:31 +0900

    done

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by Jiyeon Choi on 2022. 10. 4..
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
